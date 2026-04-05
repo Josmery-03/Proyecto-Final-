8c50f130dd346d2139d39651020915c6288cbe3f
Josmery-03 <josmery_bueno@ucne.edu.do>
2026-04-05 16:05:34 -0400

    Agregando pantalla de juego y navegacion desde el menu

--- Proyecto Final/src/app/GameApp.java
+++ Proyecto Final/src/app/GameApp.java
@@ -1,5 +1,33 @@
 package app;
 
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+import javafx.scene.control.Label;
+
 public class GameApp {
-    
+
+    private Pane root;
+
+    public GameApp(Stage stage) {
+        root = new Pane();
+        root.setStyle("-fx-background-color: black;");
+
+        Scene scene = new Scene(root, 600, 400);
+
+        stage.setTitle("Space Invaders - Juego");
+        stage.setScene(scene);
+        stage.show();
+
+        iniciar();
+    }
+
+    private void iniciar() {
+    Label texto = new Label("Aquí va el juego");
+    texto.setStyle("-fx-text-fill: white; -fx-font-size: 20px;");
+    texto.setLayoutX(200);
+    texto.setLayoutY(180);
+
+    root.getChildren().add(texto);
+    }
 }
--- Proyecto Final/src/app/MenuController.java
+++ Proyecto Final/src/app/MenuController.java
@@ -3,18 +3,25 @@ package app;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.application.Platform;
+import javafx.stage.Stage;
+import javafx.scene.Node;
 
 public class MenuController {
 
     @FXML
     private void jugar(ActionEvent event) {
         System.out.println("Iniciando juego...");
-        
+
+        // Obtener la ventana actual
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+
+        // Cambiar a la pantalla del juego
+        new GameApp(stage);
     }
 
     @FXML
     private void salir(ActionEvent event) {
         System.out.println("Saliendo...");
-        Platform.exit(); // Cierra la aplicación
+        Platform.exit();
     }
 }
