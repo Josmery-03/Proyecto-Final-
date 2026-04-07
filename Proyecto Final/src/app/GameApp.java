f1d6a242330ecdeaca69c6e3252d1fc4ebdf64e9
Josmery-03 <josmery_bueno@ucne.edu.do>
2026-04-07 17:51:56 -0400

    Implementa game loop y movimiento del jugador

--- Proyecto Final/src/app/GameApp.java
+++ Proyecto Final/src/app/GameApp.java
@@ -1,15 +1,22 @@
 package app;
 
+import javafx.animation.AnimationTimer;
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
-import javafx.scene.control.Label;
 
 public class GameApp {
 
     private Pane root;
+    private Rectangle player;
+
+    private boolean moverIzquierda = false;
+    private boolean moverDerecha = false;
 
     public GameApp(Stage stage) {
+
         root = new Pane();
         root.setStyle("-fx-background-color: black;");
 
@@ -19,15 +26,54 @@ public class GameApp {
         stage.setScene(scene);
         stage.show();
 
-        iniciar();
+        crearJugador();
+        controles(scene);
+        gameLoop();
     }
 
-    private void iniciar() {
-    Label texto = new Label("Aquí va el juego");
-    texto.setStyle("-fx-text-fill: white; -fx-font-size: 20px;");
-    texto.setLayoutX(200);
-    texto.setLayoutY(180);
+    private void crearJugador() {
+        player = new Rectangle(40, 20, Color.GREEN);
 
-    root.getChildren().add(texto);
+        player.setLayoutX(280);
+        player.setLayoutY(350);
+
+        root.getChildren().add(player);
+    }
+
+    private void controles(Scene scene) {
+
+        scene.setOnKeyPressed(e -> {
+            switch (e.getCode()) {
+                case LEFT -> moverIzquierda = true;
+                case RIGHT -> moverDerecha = true;
+            }
+        });
+
+        scene.setOnKeyReleased(e -> {
+            switch (e.getCode()) {
+                case LEFT -> moverIzquierda = false;
+                case RIGHT -> moverDerecha = false;
+            }
+        });
+    }
+
+    private void gameLoop() {
+
+        AnimationTimer timer = new AnimationTimer() {
+            @Override
+        public void handle(long now) {
+
+    if (moverIzquierda && player.getLayoutX() > 0) {
+        player.setLayoutX(player.getLayoutX() - 5);
+    }
+
+    if (moverDerecha && player.getLayoutX() < 560) {
+        player.setLayoutX(player.getLayoutX() + 5);
     }
 }
+        };
+
+        timer.start();
+    }
+}
+
